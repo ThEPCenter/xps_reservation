3d15f491d4758d2e70647dde071de4c7ba8d669d
fisca <fisca2012@gmail.com>
2014-11-24 11:12:32 +0700

    Install CodeIgniter v.2.2.0 and Bootstrap 3.3.1.

---
+++ application/controllers/admin.php
@@ -0,0 +1,40 @@
+<?php
+
+class Admin extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+
+        // ======== Library ======== //
+        $this->load->library('session');
+
+        // ======== Driver ======== //
+        $this->load->database();
+
+        // ======== Helper ======== //
+        $this->load->helper('url');
+        $this->load->helper('html');
+
+        // ======== Class ======== //
+        $this->load->library('pagination');
+        
+        /**
+          if (!($this->session->userdata('validated'))) {
+          redirect('login');
+          }
+
+          if ($this->session->userdata('level') != 10) {
+          redirect('home');
+          }
+
+          // ========= Commom Models ========= //
+          $this->load->model('admin_model');
+         * 
+         */
+    }
+
+    public function index() {
+        $this->load->view('admin/index');
+    }
+
+}
